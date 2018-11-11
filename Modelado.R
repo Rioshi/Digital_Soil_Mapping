481432b475621182add790fd9747cfaf39163bc1
Rioshi <julian.cmn@gmail.com>
2018-11-11 00:05:29 -0500

    Update Modelado.R
    aa

--- Modelado.R
+++ Modelado.R
@@ -1,7 +1,7 @@
 ##Lectura de datos##
 library(raster)
 library(caret)
-setwd("E:/TESIS/2018")
+setwd("H:/TESIS/2018")
 
 #Lectura de covariables RELIEVE
 startdir <- getwd()
@@ -15,7 +15,7 @@ setwd(startdir)
 relieve
 
 #Lectura de covariables Clima
-load("E:/TESIS/2018/RDATA/clima.RData")
+load("H:/TESIS/2018/RDATA/clima.RData")
 HS_max <- max(HS)
 HS_min <- min(HS)
 HS_acu <- sum(HS)
@@ -25,9 +25,15 @@ ppt_acu <- sum(prec)
 tmax <- mean(t_max)
 tmin <- mean(t_min)
 tmean <- mean(T_mean)
-clima <- stack(HS_max,HS_min,HS_acu,ppt_max,ppt_min,ppt_acu,tmax,tmin,tmean,
-              varname=c("HS_max","HS_min","HS_acu","ppt_max","ppt_min","ppt_acu","tmax","tmin","tmean"))
+clima <- stack(HS_max,HS_min,HS_acu,ppt_max,ppt_min,ppt_acu,tmax,tmin,tmean)
 nam <-c("HS_max","HS_min","HS_acu","ppt_max","ppt_min","ppt_acu","tmax","tmin","tmean")
+names(clima) <- nam
 rm(files,HS_max,HS_min,HS_acu,ppt_max,ppt_min,ppt_acu,tmax,tmin,tmean,i,HS,prec,
-   stack1,startdir,t_max,T_mean,t_min,BH)
+   stack1,startdir,t_max,T_mean,t_min,BH,nam)
 
+#Lectura de covariables organismos
+load("H:/TESIS/2018/RDATA/organismos.RData")
+organismos <- stack(ndvi,ci)
+nam <- c("ndvi","ci")
+names(organismos) <- nam
+rm(ci,nam,ndvi)
